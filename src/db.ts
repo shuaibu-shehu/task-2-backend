5f7c234ba77718c6059bd67c123d80e874143e4e
shuaibu-shehu <shuaibushehukalifa@gmail.com>
2024-07-10 18:23:42 +0530

    connect  script

--- src/db.ts
+++ src/db.ts
@@ -9,3 +9,12 @@ export const db = global.prisma || new PrismaClient()
 if (process.env.NODE_ENV !== 'production') {
     global.prisma = db
 }
+
+
+db.$connect()
+  .then(() => {
+    console.log('Prisma connected successfully');
+  })
+  .catch((e) => {
+    console.error('Prisma connection error', e);
+});
